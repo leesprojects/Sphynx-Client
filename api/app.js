cc14550c080f5862bd13fe7cb6cfadd858cddde0
leesprojects <leepeterstevens@gmail.com>
2023-12-02 16:08:27 +0000

    Initialised an Angular 17 client with an ExpressJS 20 Server - Ready for development

---
+++ api/app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//C onfigurations
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Error Handling
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', { message: err.message, status: err.status, stackTrace: err.stack});
+});
+
+
+
+let port = process.env.PORT;
+if (!port || port == '') port = '8000';
+
+app.listen(port, function() {
+  console.log(`Project Sphynx API listening at http://localhost:${port}`);
+});
+
+module.exports = app;
