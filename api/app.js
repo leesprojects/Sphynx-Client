626fe50797cef1533334485968561c457264ade2
leesprojects <leepeterstevens@gmail.com>
2024-01-15 13:39:30 +0000

    Upgraded Angular & Node, added favicon support for node

--- api/app.js
+++ api/app.js
@@ -1,26 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const favicon = require('serve-favicon');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//C onfigurations
+//Configurations
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // Routes
 app.use('/', indexRouter);
@@ -33,7 +32,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -42,8 +41,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', { message: err.message, status: err.status, stackTrace: err.stack});
 });
-
-
 
 let port = process.env.PORT;
 if (!port || port == '') port = '8000';
--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
 # Project Sphynx
 
 ## Description
-This is the client-side application for Project Sphynx.
+A multi-purpose full-stack web application used for various goals and needs in my daily life.
+The primary and initial feature will be a Spanish Study feature, mainly consisting of a flashcard-like system.
+
+## Specification
+An Angular - ExpressJS application utilising a relational SQL database.
+The client is hosted via Netlify, while the backend is not currently setup 
 
 ## About
+ 
