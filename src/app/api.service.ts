f03b97e3754fc98d64a56bf68971b734afb7acba
leesprojects <leepeterstevens@gmail.com>
2024-02-29 23:53:01 +0000

    Further information from api service and handshake

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,29 +3,25 @@ import { environment } from "environments/environment";
 
 @Injectable({ providedIn: 'root' })
 export class APIService {
-  apiUrl = process.env['NG_APP_API_URL'] as string;
-  apiUrlFromEnv = environment.ENV_NAME;
-  envName = process.env['NG_APP_ENV_NAME'] as string;
+  apiUrl = environment.API_URL as string;
+  envName = environment.ENV_NAME as string;
+  apiAvailable = false;
 
   constructor() {
-    console.log('API URL:', this.apiUrl, ` for Environment: ${this.envName}, the EnvName is ${this.apiUrlFromEnv}`)
     this.apiHandshake();
   }
 
   private apiHandshake(): Promise<void> {
     return fetch(this.apiUrl).then(response => {
-      if (response.status === 200) {
-        console.info('API handshake: success');
-      } else if (!response.ok) {
-        throw new Error('API handshake failed');
-      }
+      this.apiAvailable = response.status === 200;
+      console.info('API:', this.apiUrl, `\nENV: ${this.envName}\nHandshake: ${response.status}\nAPI Available: ${this.apiAvailable ? 'Yes' : 'No'}`);
     });
   }
 
   get(endpoint: string): Promise<any> {
     const getUrl = `${this.apiUrl}${endpoint}`;
     return fetch(getUrl).then(res => {
-      console.info(`GET ${this.apiUrl}${endpoint}: success`);
+      console.info(`GET ${endpoint}: success`);
       return Promise.resolve(res.json())
     }).catch(() => {
       console.info(`GET ${endpoint}: failed`);
