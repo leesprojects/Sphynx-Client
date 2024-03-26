3194cef63689ed6faf55519868cf11ab76f2ea7a
Lee Stevens <lee.stevens@atomus.com>
2024-03-26 14:02:18 +0000

    WIP Exercise Cards drag'n'drop categories

--- src/app/@features/trainer-library/exercises/exercises-table/exercises-table.component.ts
+++ src/app/@features/trainer-library/exercises/exercises-table/exercises-table.component.ts
@@ -1,4 +1,5 @@
 import { IExercise } from '@App/@models/trainer';
+import { CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, Input } from '@angular/core';
 
 export type ExerciseTableView = 'table' | 'card';
@@ -12,4 +13,28 @@ export class ExercisesTableComponent {
   @Input({ required: true }) view: ExerciseTableView;
   @Input({ required: true }) exercises: IExercise[];
   @Input({ required: true }) columnsToRender: string[];
+
+  exerciseTitle: string[] = ['A', 'B', 'C'];
+  exerciseDropList: IExercise[] = [
+    {
+      id: 1,
+      name: 'Bench Press',
+      description: 'The bench press is an upper-body weight training exercise in which the trainee presses a weight upwards while lying on a weight training bench.',
+      category: 'Calisthenics'
+    }
+  ];
+  workoutExerciseDropList: string[] = ['D'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
